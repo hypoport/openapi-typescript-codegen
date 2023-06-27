27efb8ff833930c6fd0984250c750c02f678c2d0
Dennis Wehrle <dennis.wehrle@ampr.de>
2023-06-27 17:22:59 +0200

    added release workflow

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from 'fs';
 import { sync } from 'glob';
 
-import { generate, HttpClient } from '../';
+import { generate, HttpClient, Indent } from '../';
 
 describe('v2', () => {
     it('should generate', async () => {
@@ -41,6 +41,25 @@ describe('v3', () => {
         sync('./test/generated/v3/**/*.ts').forEach(file => {
             const content = readFileSync(file, 'utf8').toString();
             expect(content).toMatchSnapshot(file);
+        });
+    });
+});
+
+describe('homecloud', () => {
+    it('should generate', async () => {
+        await generate({
+            clientName: 'HomecloudApiService',
+            exportCore: false,
+            exportModels: false,
+            exportSchemas: false,
+            exportServices: true,
+            httpClient: HttpClient.AXIOS,
+            indent: Indent.SPACE_2,
+            input: 'https://test.docs.api.homecloud.de/homecloud-api-specs.json',
+            output: './test/generated/homecloud/',
+            postfixServices: 'ApiService',
+            useOptions: false,
+            useUnionTypes: false,
         });
     });
 });
